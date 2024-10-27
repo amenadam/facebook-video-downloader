af3afaa5a1ed1b36ebb98d1fc89b01b8c1bf174b
amenadam <aminadamsolomon5@gmail.com>
2024-10-27 11:22:07 +0300

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,1 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const request = require("request");
+const app = express();
+
+app.use(express.json());
+
+const getUrl = (url) => {
+  return new Promise((resolve, reject) => {
+    let options = {
+      method: "POST",
+      url: "https://www.getfvid.com/downloader",
+      formData: { url: url },
+    };
+
+    request(options, function (error, response) {
+      if (error) {
+        console.error("Error fetching video:", error);
+        reject("Error fetching video: " + error);
+        return;
+      }
+
+      const rgx =
+        /<a href="(.+?)" target="_blank" class="btn btn-download"(.+?)>(.+?)<\/a>/g;
+      let arr = [...response.body.matchAll(rgx)];
+      if (arr.length > 0) {
+        resolve({ videoName: "video.mp4", downloadUrl: arr[0][1] });
+      } else {
+        reject("No download link found.");
+      }
+    });
+  });
+};
+
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    const url = req.body.url;
+    try {
+      const { videoName, downloadUrl } = await getUrl(url);
+      res.json({ videoName, downloadUrl });
+    } catch (error) {
+      console.error("Error in API route:", error);
+      res.status(500).json({ error });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+};
 
